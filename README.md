77bc8ab49ba60a240f6d91d147999b924d731894
Alexander Merkulov <sasha@merqlove.ru>
2014-07-17 04:50:52 +0400

    README update. Options help update. Added quiet mode. Added putting API key from console

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # DoSnapshot
 
-TODO: Write a gem description
+You can use this gem to backup's DigitalOcean droplet's via snapshot.
+
+Here some features:
+
+- Binary for cron and command-line.
+- Mail notification when maximum of snapshots for droplet reached.
+- Custom mail settings (You can set [Pony](https://github.com/benprew/pony) mail settings).
+- Stop mode (when you don't want to create new snapshots when maximum is reached).
+- Logging into selected directory.
+- Trace mode for research.
+- Quiet mode for silence.
+
+There not so much of dependencies:
+
+- `Digitalocean` for API requests.
+- `Thor` for CLI.
+- `Pony` for mail notifications.
 
 ## Installation
 
@@ -18,7 +34,60 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+First you may need to set DigitalOcean API keys:
+ 
+```shell
+  export DIGITAL_OCEAN_CLIENT_ID = "SOMEID"
+  export DIGITAL_OCEAN_API_KEY = "SOMEKEY"
+``` 
+If you want to set keys without environment, than set it via options:
+
+```shell
+  do_snapshot --digital-ocean-client-id YOURLONGAPICLIENTID --digital-ocean-api-key YOURLONGAPIKEY
+```
+
+E-mail notifications disabled out of the box. 
+For working mailer you need to set e-mail settings via run options.
+
+```shell
+Usage:
+  do_snapshot create
+
+Options:
+  -o, [--only=123456 123456 123456]                              # Use only selected droplets.
+  -e, [--exclude=123456 123456 123456]                           # Except some droplets.
+  -k, [--keep=5]                                                 # How much snapshots you want to keep?
+                                                                 # Default: 10
+  -s, [--stop], [--no-stop]                                      # Stop creating snapshots if maximum is reached.
+  -m, [--mail=to:yourmail@example.com]                           # Receive mail if maximum is reached.
+  -t, [--smtp=user_name:yourmail@example.com password:password]  # SMTP options.
+  -l, [--log=/Users/someone/.do_snapshot/main.log]               # Log file path. By default logging is disabled.
+  -d, [--trace], [--no-trace]                                    # Debug mode.
+  -q, [--quiet], [--no-quiet]                                    # Quiet mode. If don't need any messages and log's
+      [--digital-ocean-client-id=YOURLONGAPICLIENTID]            # DIGITAL_OCEAN_CLIENT_ID. if you can't use environment.
+      [--digital-ocean-api-key=YOURLONGAPIKEY]                   # DIGITAL_OCEAN_API_KEY. if you can't use environment.
+
+Description:
+  `do_snapshot create` will create and cleanup snapshots on your droplets.
+
+  You can optionally specify parameters to select or exclude some droplets.
+
+  Advanced options example for MAIL feature:
+
+  --mail to:mail@somehost.com from:from@host.com --smtp address:smtp.gmail.com user_name:someuser password:somepassword
+
+  For more details look here: https://github.com/benprew/pony
+
+  Example:
+
+  > $ do_snapshot --keep 5
+
+  > $ do_snapshot --only 123456 1234567 --store 3
+
+  > $ do_snapshot --exclude 123456 123457
+
+  > $ do_snapshot --keep 10 --stop true --mail to:yourmail@example.com
+```
 
 ## Contributing
 
--- lib/do_snapshot/cli.rb
+++ lib/do_snapshot/cli.rb
@@ -10,11 +10,16 @@ module DoSnapshot
 
     def initialize(*args)
       super
-      if options.include? 'log'
+      if options.include?('log') && !options.include?('quiet')
         Log.logger       = Logger.new(options['log'])
         Log.thor_log     = Thor::Shell::Color.new
         Log.logger.level = options.include?('trace') ? Logger::DEBUG : Logger::INFO
       end
+
+      # Check for keys via options
+      ENV['DIGITAL_OCEAN_CLIENT_ID'] = options['digital_ocean_client_id'] if options.include? 'digital_ocean_client_id'
+      ENV['DIGITAL_OCEAN_API_KEY']   = options['digital_ocean_api_key'] if options.include? 'digital_ocean_api_key'
+
       try_keys_first
     end
 
@@ -43,13 +48,17 @@ module DoSnapshot
     option :only, type: :array, default: [], aliases: ['-o'], banner: '123456 123456 123456', desc: 'Use only selected droplets.'
     option :exclude, type: :array, default: [], aliases: ['-e'], banner: '123456 123456 123456', desc: 'Except some droplets.'
     option :keep, type: :numeric, default: 10, aliases: ['-k'], banner: '5', desc: 'How much snapshots you want to keep?'
-    option :stop, type: :boolean, aliases: ['-s'], banner: 'true', desc: 'Stop creating snapshots if maximum is reached.'
+    option :stop, type: :boolean, aliases: ['-s'], desc: 'Stop creating snapshots if maximum is reached.'
     option :mail, type: :hash, default: {}, aliases: ['-m'], banner: 'to:yourmail@example.com', desc: 'Receive mail if maximum is reached.'
     option :smtp, type: :hash, default: {}, aliases: ['-t'], banner: 'user_name:yourmail@example.com password:password', desc: 'SMTP options.'
     option :log, type: :string, aliases: ['-l'], banner: '/Users/someone/.do_snapshot/main.log', desc: 'Log file path. By default logging is disabled.'
-    option :trace, type: :boolean, aliases: ['-d'], desc: 'Debug mode. Not implented now. Switched only log mode.'
+    option :trace, type: :boolean, aliases: ['-d'], desc: 'Debug mode.'
+    option :quiet, type: :boolean, aliases: ['-q'], desc: 'Quiet mode. If don\'t need any messages and log\'s'
+    option :digital_ocean_client_id, type: :string, banner: 'YOURLONGAPICLIENTID', desc: 'DIGITAL_OCEAN_CLIENT_ID. if you can\'t use environment.'
+    option :digital_ocean_api_key, type: :string, banner: 'YOURLONGAPIKEY', desc: 'DIGITAL_OCEAN_API_KEY. if you can\'t use environment.'
+
     def create
-      DoSnapshot::Command.execute options, %w( log trace )
+      DoSnapshot::Command.execute options, %w( log trace quiet digital_ocean_client_id digital_ocean_api_key )
     rescue => e
       Log.error e.message
       backtrace(e) if options.include? 'trace'
@@ -63,11 +72,11 @@ module DoSnapshot
       end
     end
 
-    # Check DigitalOcean API keys
+    # Check for DigitalOcean API keys
     def try_keys_first
       Log.debug 'Checking DigitalOcean Id\'s.'
-      fail Thor::Error, 'You must have DIGITAL_OCEAN_CLIENT_ID in environment.' if !ENV['DIGITAL_OCEAN_CLIENT_ID'] || ENV['DIGITAL_OCEAN_CLIENT_ID'].empty?
-      fail Thor::Error, 'You must have DIGITAL_OCEAN_API_KEY in environment.' if !ENV['DIGITAL_OCEAN_API_KEY'] || ENV['DIGITAL_OCEAN_API_KEY'].empty?
+      fail Thor::Error, 'You must have DIGITAL_OCEAN_CLIENT_ID in environment or set via options.' if !ENV['DIGITAL_OCEAN_CLIENT_ID'] || ENV['DIGITAL_OCEAN_CLIENT_ID'].empty?
+      fail Thor::Error, 'You must have DIGITAL_OCEAN_API_KEY in environment or set via options.' if !ENV['DIGITAL_OCEAN_API_KEY'] || ENV['DIGITAL_OCEAN_API_KEY'].empty?
     end
   end
 end
