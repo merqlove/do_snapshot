a686c3d7606904cf1aa9e5830a5fcd07b8d5c8cf
Alexander Merkulov <sasha@merqlove.ru>
2016-01-09 14:57:09 +0300

    Use autoload instead of require. Fix adapter getter.

--- lib/do_snapshot/adapter.rb
+++ lib/do_snapshot/adapter.rb
@@ -1,18 +1,17 @@
-require_relative 'adapter/abstract'
-
-require_relative 'adapter/digitalocean_v2'
-
 module DoSnapshot
   # Adapter interface for API connections
   # Ability to select DigitalOcean API versions.
   #
   module Adapter
+    autoload :Abstract, 'do_snapshot/adapter/abstract'
+    autoload :DigitaloceanV2, 'do_snapshot/adapter/digitalocean_v2'
+
     class << self
       def api(protocol, options = {})
         konst = find_protocol(protocol)
         fail DoSnapshot::NoProtocolError, "Not existing protocol: #{protocol}." unless
-            Object.const_defined?(konst)
-        obj = Object.const_get(konst)
+            DoSnapshot::Adapter.const_defined?(konst)
+        obj = DoSnapshot::Adapter.const_get(konst)
         obj.new(options)
       end
 
@@ -20,11 +19,11 @@ module DoSnapshot
 
       def find_protocol(protocol)
         if protocol.is_a?(Integer)
-          "DoSnapshot::Adapter::DigitaloceanV#{protocol}"
+          "::DoSnapshot::Adapter::DigitaloceanV#{protocol}"
         elsif protocol.is_a?(String)
           protocol
         else
-          'DoSnapshot::Adapter::DigitaloceanV2'
+          '::DoSnapshot::Adapter::DigitaloceanV2'
         end
       end
     end
