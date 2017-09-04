78610caf9dfb2fb687d4d0416e934cc5f793641b
Alexander Merkulov <sasha@merqlove.ru>
2017-09-05 00:44:04 +0300

    Update new_adapter_spec.rb

--- example/adapter/spec/new_adapter_spec.rb
+++ example/adapter/spec/new_adapter_spec.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe DoSnapshot::Adapter::NewAdapter do
+RSpec.describe NewAdapter do
   include DoSnapshot::RSpec::Environment
   include DoSnapshot::RSpec::ApiV2Helpers
   include DoSnapshot::RSpec::Adapter
